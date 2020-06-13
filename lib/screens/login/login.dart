ea07786ef8675d117c6627269a0d79ae330c99f8
matheus nunes <matheusnunes@localhost.localdomain>
2020-06-13 03:55:28 -0300

    Tela de login

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'loginForm.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("App Bar"),
-      ),
-      body: Text("Login"),
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/fundo_login.png"),
+              fit: BoxFit.cover
+            )
+          ),
+        ),
+        LoginForm()
+      ],
     );
   }
 }
+
---
+++ lib/screens/login/loginForm.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+class _LoginFormState extends State<LoginForm>{
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context){
+    final phoneField = TextField(
+      obscureText: false,
+      style: TextStyle(color: Color(0xFFbdc6cf)),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
+        hintText: "E-mail/Telefone",
+        border: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+      ),
+    );
+
+    final passField = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
+        hintText: "E-mail/Telefone",
+        filled: true,
+        fillColor: Colors.white,
+      ),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width-20,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+
+        onPressed: () {},
+
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final createAccountBtn = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width-30,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+
+        onPressed: () {},
+
+        child: Text("Criar conta",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Container(
+          color: Colors.transparent,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 155.0,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 45.0),
+                phoneField,
+                SizedBox(height: 25.0),
+                passField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+                createAccountBtn
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
